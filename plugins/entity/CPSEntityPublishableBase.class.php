52d52d8525315ff6b00349de9d49b396b46edf61
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2014-12-04 13:03:51 +0000

    Drafty refactor

--- plugins/entity/CPSEntityPublishableBase.class.php
+++ plugins/entity/CPSEntityPublishableBase.class.php
@@ -55,28 +55,47 @@ abstract class CPSEntityPublishableBase extends CPSEntityBase implements CPSEnti
   /**
    * @{inheritdoc}
    */
-  public function createUnpublishedRevision($entity) {
-    // This contains only general code --
-    // children will need to implement this and then call up to this.
-    list($entity_id, $revision_id,) = entity_extract_ids($this->entity_type, $entity);
-
-    db_insert('cps_entity')
-      ->fields(array(
-        'entity_type' => $this->entity_type,
-        'entity_id' => $entity_id,
-        'changeset_id' => 'initial',
-        'revision_id' => $revision_id,
-      ))
-      ->execute();
-  }
-
-  /**
-   * @{inheritdoc}
-   */
   public function hook_entity_insert($entity) {
     parent::hook_entity_insert($entity);
     if ($this->getCurrentChangeset()) {
       $this->createUnpublishedRevision($entity);
     }
+  }
+
+  /**
+   * Create a published revision in the current changeset.
+   *
+   * When an new entity is inserted, we actually want two revisions to be saved.
+   *  - An unpublished revision in the 'published' changeset, since CPS treats
+   *    any entity that's never been published as 'unpublished'.
+   *  - A draft revision in the current changeset with the published flag set to
+   *    the same as on the node form.
+   *
+   * This method exists as a parent method for children that will implement
+   * CPSEntityPublishableInterface to share common code. It assumes that
+   * anything that calls this will inherit that interface, and will crash
+   * if that is not true.
+   *
+   * When PHP 5.4 is reliable, we can use a trait to enforce this instead.
+   *
+   * See CPSEntityPublishableInterface::createUnpublishedRevision()
+   */
+  public function createUnpublishedRevision($original_entity) {
+    /** @var Entity $entity */
+    // This will only get called if an object subclassing this implements
+    // CPSEntityPublishableInterface which will make these methods valid.
+    $changeset = $this->getCurrentChangeset();
+    if (!$changeset) {
+      return;
+    }
+    // Operate on a clone to avoid changing the entity prior to subsequent
+    // hook_entity_insert() implementations.
+    $published_entity = clone $original_entity;
+
+    // The entity is no longer new.
+    unset($published_entity->is_new);
+
+    $this->unpublish($published_entity);
+    entity_save($this->entity_type, $published_entity);
   }
 }
