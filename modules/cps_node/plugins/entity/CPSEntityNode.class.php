5986219576e7ce683fdab74ac259f147470dd2ce
Nathaniel Catchpole <catch@35733.no-reply.drupal.org>
2014-12-04 13:03:51 +0000

    Drafty refactor

--- modules/cps_node/plugins/entity/CPSEntityNode.class.php
+++ modules/cps_node/plugins/entity/CPSEntityNode.class.php
@@ -22,45 +22,6 @@ class CPSEntityNode extends CPSEntityPublishableBase {
   /**
    * @{inheritdoc}
    */
-  public function restoreEntityData($entity) {
-    parent::restoreEntityData($entity);
-    if (!isset($entity->published_revision)) {
-      // This should always have been set on load, if not, just return.
-      return;
-    }
-
-    drupal_write_record('node', $entity->published_revision, 'nid');
-  }
-
-  /**
-   * @{inheritdoc}
-   */
-  public function createUnpublishedRevision($original_entity) {
-    // Do all work on a clone!
-    $entity = clone $original_entity;
-    $entity->original = $original_entity;
-    $this->unpublish($entity);
-    $entity->log = t('Initial live revision');
-    $entity->vid = NULL;
-    _node_save_revision($entity, $entity->uid);
-    // Make sure the field data for the live revision gets written so that it is
-    // not out of sync.
-    $entity->revision = TRUE;
-    // This is really a special case.
-    $entity->cps_initial_revision = TRUE;
-    // Remove all field collections from the first unpublished revision.
-    $this->fieldCollectionRemove($entity);
-    field_attach_update($this->entity_type, $entity);
-    db_update('node')
-      ->fields(array('vid' => $entity->vid, 'status' => NODE_NOT_PUBLISHED))
-      ->condition('nid', $entity->nid)
-      ->execute();
-    parent::createUnpublishedRevision($entity);
-  }
-
-  /**
-   * @{inheritdoc}
-   */
   public function isPublished($entity) {
     return $entity->status != NODE_NOT_PUBLISHED;
   }
--- plugins/entity/CPSEntityAPIBase.class.php
+++ plugins/entity/CPSEntityAPIBase.class.php
@@ -9,55 +9,14 @@
  */
 class CPSEntityAPIBase extends CPSEntityBase {
   /**
-   * @{inheritdoc}
-   */
-  public function setRevisionFlag($entity, $set_log = TRUE) {
-    // Entities that uses alternative methods should override this.
-    $entity->is_new_revision = TRUE;
-    $entity->default_revision = FALSE;
-
-    if ($set_log) {
-      $changeset = cps_changeset_load($this->getCurrentChangeset());
-      if (is_object($changeset)) {
-        $name = $changeset->name;
-      }
-      else {
-        $name = $this->getCurrentChangeset() ? $this->getCurrentChangeset() : 'Published';
-      }
-      $entity->log = t('Revision for site version %changeset', array('%changeset' => $name));
-    }
-  }
-
-  /**
-   * @{inheritdoc}
-   */
-  public function unsetRevisionFlag($entity) {
-    $entity->is_new_revision = FALSE;
-  }
-
-  /**
-   * @{inheritdoc}
-   */
-  public function restoreEntityData($entity) {
-    // Entity API will put fields back if editing an existing revision but not
-    // if a new revision was created.
-    if (isset($entity->published_revision)) {
-      // Ensure file fields don't do funny business. It uses $entity->original
-      // to determine if it has to add/remove file usages, and NOT setting this
-      // confuses it very badly.
-      $entity->published_revision->original = $entity->published_revision;
-
-      if (!empty($entity->revision)) {
-        field_attach_update($this->entity_type, $entity->published_revision);
-      }
-      else {
-        $entity->original = $entity->published_revision;
-      }
-    }
-  }
-
-  /**
-   * Create an unpublished revision.
+   * Create a published revision in the current changeset.
+   *
+   * When an new entity is inserted, we actually want two revisions to be saved.
+   *  - An unpublished revision in the 'published' changeset, since CPS treats
+   *    any entity that's never been published as 'unpublished'.
+   *  - A draft revision in the current changeset with the published flag set to
+   *  TRUE, this will allow the entity to show up in previews, and for that flag
+   *  to be set correctly when the changeset is eventually published.
    *
    * This method exists as a parent method for children that will implement
    * CPSEntityPublishableInterface to share common code. It assumes that
@@ -72,56 +31,46 @@ class CPSEntityAPIBase extends CPSEntityBase {
     /** @var Entity $entity */
     // This will only get called if an object subclassing this implements
     // CPSEntityPublishableInterface which will make these methods valid.
+    $changeset = $this->getCurrentChangeset();
+    if (!$changeset) {
+      return;
+    }
+    // Operate on a clone to avoid changing the entity prior to subsequent
+    // hook_entity_insert() implementations.
+    $published_entity = clone $original_entity;
 
-    // Do all work on a clone!
-    $entity = clone($original_entity);
-    $entity->original = $original_entity;
-    $this->unpublish($entity);
-    $entity->log = t('Initial live revision');
-    $entity->is_new_revision = TRUE;
-    $entity->default_revision = TRUE;
-    // NOTE: EntityAPIController::saveRevision() is protected, but we need
-    // to access it. We use this trick to create a subclass and a static
-    // method that can effectively fill the gap left by PHP not having
-    // a 'friend' keyword.
-    CPSEntityAPIControllerFriend::protectedSaveRevision(entity_get_controller($entity->entityType()), $entity);
-    // Make sure the field data for the live revision gets written so that it is not
-    // out of sync.
+    // The entity is no longer new.
+    unset($published_entity->is_new);
 
-    $entity->revision = TRUE;
-    // This is really a special case.
-    $entity->cps_initial_revision = TRUE;
-    // Remove all field collections from the first unpublished revision.
-    $this->fieldCollectionRemove($entity);
-
-    field_attach_update($this->entity_type, $entity);
-
-    // class Entity provides identifier() but does not provide a similar method for
-    // accessing the revision id, so we must use the old entity_extract_ids() here.
-    list($entity_id, $revision_id,) = entity_extract_ids($this->entity_type, $entity);
-    db_insert('cps_entity')
-      ->fields(array(
-        'entity_type' => $this->entity_type,
-        'entity_id' => $entity_id,
-        'changeset_id' => 'initial',
-        'revision_id' => $revision_id,
-      ))
-      ->execute();
+    $this->unpublish($published_entity);
+    entity_save($this->entity_type, $published_entity);
   }
 
   /**
    * @{inheritdoc}
    */
   public function hook_entity_insert($entity) {
-    // This exists to make it simpler for subclasses to support publishing
-    // and Entity API without multiple inheritance.
-    parent::hook_entity_insert($entity);
-    // We use method_exists rather than is_subclass_of because is_subclass_of
-    // does not seem to check interfaces on classes inheriting and so that
-    // always returns false.
-    if (method_exists($this, 'publish') && $this->getCurrentChangeset()) {
-      $this->createUnpublishedRevision($entity);
+    if ($changeset_id = $this->getCurrentChangeset()) {
+      // Store state information.
+      // When an entity supports publishing, the first revision has to be stored
+      // unpublished so that it does not show up on the live site. However, in
+      // the current changeset for previewing and eventual publishing, it should
+      // be set to published. Hence for those entity types, store the first
+      // revision in the special 'initial' changeset unpublished, then
+      // immediately create a draft revision in the current changeset.
+      list($entity_id, $revision_id) = entity_extract_ids($this->entity_type, $entity);
+      db_insert('cps_entity')
+        ->fields(array(
+          'entity_type' => $this->entity_type,
+          'entity_id' => $entity_id,
+          'changeset_id' => $changeset_id,
+          'revision_id' => $revision_id,
+        ))->execute();
+      // does not seem to check interfaces on classes inheriting and so that
+      // always returns false.
+      if (method_exists($this, 'publish')) {
+        $this->createUnPublishedRevision($entity);
+      }
     }
   }
-
 }
